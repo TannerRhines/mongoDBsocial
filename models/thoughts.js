5a993248e1576b7d56973590e5c7627ea696b964
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-12 04:17:59 -0800

    created thoughts.js schema

--- models/thoughts.js
+++ models/thoughts.js
@@ -1,8 +1,41 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const { Schema, model } = require('mongoose');
 
-router.use('/api', apiRoutes);
+// scheme for thought 
+const thoughtSchema = new Schema (
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        // makes the data readable 
+        get: createdAtVal => dateFormat(createdAtVal)
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+)
 
-router.use((req, res) => res.send('Wrong route!'));
+// get total count of friends
+thoughtSchema.virtual('reactionCount')
+.get(function() {
+    return this.reactions.length;
+})
 
-module.exports = router;
+// create the User model using the UserSchema
+const Thought = model('Thought', thoughtSchema);
+// export the Thought model
+module.exports = Thought;
