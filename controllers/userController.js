5b8422e47414405912ab6a865410f573a1a49031
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-12 05:10:13 -0800

    created getSingleUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,3 +22,23 @@ getUser = (req, res) => {
         res.status(500).json({ message: "An error occurred while fetching users.", error: err.message });
       });
   };
+
+
+
+  getSingleUser = (req, res) => {
+    const userId = req.params.userId;
+  
+    User.findOne({ _id: userId })
+      .select('-__v')
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({ message: 'No user found with that ID!' });
+        }
+  
+        return res.json(user);
+      })
+      .catch((err) => {
+        console.error(err); // Log the error for debugging
+        return res.status(500).json({ message: 'An error occurred while fetching the user.', error: err.message });
+      });
+  };
