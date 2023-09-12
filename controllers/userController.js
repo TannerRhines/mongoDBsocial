2ffabbbed69bf9cd530bc10eb3fdb13e1243e226
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-12 05:08:27 -0800

    created getUser function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,24 @@
 const { User, Thought } = require('../models');
+
+
+
+
+
+// get all the users in DB
+
+
+
+getUser = (req, res) => {
+    User.find()
+      .select("-__v") // Excludes the __v field
+      .then((allUsers) => {
+        if (!allUsers.length) {
+          return res.status(404).json({ message: "No users found." });
+        }
+        res.json(allUsers);
+      })
+      .catch((err) => {
+        console.error(err); // Log the error for debugging
+        res.status(500).json({ message: "An error occurred while fetching users.", error: err.message });
+      });
+  };
