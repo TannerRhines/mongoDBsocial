7472566898bf6d6d7c6d7d0fc410ee662ea7f093
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-11 22:51:59 -0800

    finished user model

--- models/user.js
+++ models/user.js
@@ -8,4 +8,44 @@ const userSchema = new Schema(
         required: true,
         trim: true,
       },
-    })
+    
+    email: {
+        type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Thought",
+        },
+      ],
+      friends: [
+        { 
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+      ],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }
+  );
+ 
+
+
+userSchema.virtual("friendCount").get(function () {
+    return $this.friends.length;
+  });
+  
+  // creates User model using the UserSchema
+  const User = model('User', userSchema);
+  // export the User model
+  module.exports = User;
