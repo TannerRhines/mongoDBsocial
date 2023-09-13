797d75d7d199573bc245d413c6cae545964e2ab3
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-13 00:41:44 -0800

    createThought added

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -47,6 +47,34 @@ getSingleThought = (req, res) => {
 // create thought
 
 
+createThought = (req, res) => {
+
+    // create  new Thought 
+    Thought.create(req.body)
+      .then(({ _id }) => {
+        // Once the Thought is created, update the User with the new Thought's ID
+        return User.findOneAndUpdate(
+          { _id: req.params.userId },  // Find user by ID
+          { $push: { thoughts: _id } },  // push the new Thought ID into the user's thoughts array
+          { new: true }  // Return the updated User object
+        );
+      })
+      .then((thought) => {
+        // check if the user was found and updated
+        if (!thought) {
+          res.status(404).json({ message: 'No User found with this ID!' });
+        } else {
+          res.json(thought);
+        }
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  };
+
+  
+  
+
 
 
 
