466ed2bfa8fb3f4ffc1c018c73d0a2a4b6c5b0a9
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-13 01:52:34 -0800

    fixed syntax error with models index

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const User = require('./User');
-const Thought = require('./Thought');
+const User = require('./user');
+const Thought = require('./thoughts');
 
 module.exports = { User, Thought };
