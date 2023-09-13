0f1c367d86ba7c9b79c60f49d89ea15a2196de5c
Tanner Rhines <tannerrhines@protonmail.com>
2023-09-13 01:44:14 -0800

    debugging routes

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+// Log apiRoutes to the console for debugging
+console.log(apiRoutes);
+
+router.use('/api', apiRoutes);
+router.use((req, res) => res.send('Wrong route!'));
+
+module.exports = router;
